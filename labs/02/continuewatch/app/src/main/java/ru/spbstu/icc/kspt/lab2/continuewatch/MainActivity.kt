34d08cb68bb070a901cf0e7cccef1efa9dbc9de0
Sergei Anciferov <sergozas@yandex.ru>
2021-10-10 18:56:15 +0300

    1

--- labs/02/continuewatch/app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
+++ labs/02/continuewatch/app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.spbstu.icc.kspt.lab2.continuewatch
-
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -7,12 +8,16 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     var secondsElapsed: Int = 0
     lateinit var textSecondsElapsed: TextView
+    var onScreen = true
+    private lateinit var sharedPref: SharedPreferences
 
     var backgroundThread = Thread {
         while (true) {
-            Thread.sleep(1000)
-            textSecondsElapsed.post {
-                textSecondsElapsed.setText("Seconds elapsed: " + secondsElapsed++)
+            if (onScreen) {
+                Thread.sleep(1000)
+                textSecondsElapsed.post {
+                    textSecondsElapsed.text = getString(R.string.text, secondsElapsed++)
+                }
             }
         }
     }
@@ -21,6 +26,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textSecondsElapsed = findViewById(R.id.textSecondsElapsed)
+        sharedPref = getSharedPreferences(SECONDS_ELAPSED, Context.MODE_PRIVATE)
         backgroundThread.start()
     }
+
+    override fun onStart() {
+        onScreen = true
+        secondsElapsed = sharedPref.getInt(SECONDS_ELAPSED, secondsElapsed)
+        super.onStart()
+    }
+
+    override fun onStop() {
+        onScreen = false
+        with(sharedPref.edit()) {
+            putInt(SECONDS_ELAPSED, secondsElapsed)
+            apply()
+        }
+        super.onStop()
+    }
+
+    companion object { const val SECONDS_ELAPSED = "Seconds elapsed" }
+
 }
